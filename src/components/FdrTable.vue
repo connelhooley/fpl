4d1c7b44ef864ec31f05c78325877faa1ec7516b
Connel Hooley <connelhooley@gmail.com>
2021-03-02 21:39:51 +0000

    Tweaked table page ready for spinner

--- src/components/FdrTable.vue
+++ src/components/FdrTable.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Table to go here</div>
+  <div v-if="loaded">Table to go here</div>
+  <div v-else>Loading...</div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      loaded: false,
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.loaded = true;
+    }, 2000);
+  },
+};
+</script>
